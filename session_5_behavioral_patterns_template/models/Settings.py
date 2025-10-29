634a1ee467e061aec18a6747301552a10d789959
Kristeris <98512081+Kristeris@users.noreply.github.com>
2025-10-29 19:53:44 +0200

    Delete session_5_behavioral_patterns_template directory

--- session_5_behavioral_patterns_template/models/Settings.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from dataclasses_json import dataclass_json
-
-
-@dataclass_json
-@dataclass
-class Settings:
-    player_name: str = "Player"
--- session_5_behavioral_patterns_template/models/enums/EnumGameObjectDirection.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class EnumGameObjectDirection(str, Enum):
-    Up = "Up"
-    Down = "Down"
-    Left = "Left"
-    Right = "Right"
-    NotSet = "NotSet"
